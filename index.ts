848ef0f664e165ffa092fe88ae1e5221ccb46f8e
Aryan Jabbari <AryanJabbari@gmail.com>
2023-01-12 14:54:00 -0500

    feat: initial commit

---
+++ index.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import fs from 'fs';
+import mjml2html from 'mjml';
+import util from 'util';
+import MailGunClient from './services/MailgunClient';
+// @ts-expect-error
+import mjmlBulletList from 'mjml-bullet-list';
+
+const readDir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+
+dotenv.config();
+
+const domains = [
+  'sandboxa0e011abaa70432c9e8dbcbf44304ff2.mailgun.org', // dev
+  process.env.NODE_ENV !== 'development' ? 'mg.pepeflyers.com' : '', // prod
+].filter(Boolean);
+
+async function main() {
+  const mjmlFileNames = await readDir('./templates');
+
+  domains.forEach((d) => {
+    const mailgunClient = new MailGunClient(d);
+    console.log(`uploading to ${d}...`);
+    mjmlFileNames.forEach(async (fileName) => {
+      console.log(`uploading ${fileName}`);
+      const mjml = await getFileContents(`./templates/${fileName}`);
+      const { html } = mjml2html(mjml);
+      const [templateName] = fileName.split('.');
+      await mailgunClient.upsertTemplate(templateName, templateName, 'initial', html);
+      console.log(`${fileName} uploaded`);
+    });
+  });
+}
+
+async function getFileContents(filePath: string) {
+  const fileBuffer = await readFile(filePath);
+  return fileBuffer.toString();
+}
+
+main();
---
+++ services/MailgunClient.ts
@@ -0,0 +1,43 @@
+import Mailgun from 'mailgun-js';
+
+export default class MailGunClient extends Mailgun {
+  constructor(private readonly domain: string) {
+    super({ apiKey: process.env.MAILGUN_API_KEY, domain });
+  }
+
+  private postTemplate = async (
+    templateName: string,
+    description: string,
+    htmlTemplate: string
+  ) => {
+    return this.post(`/${this.domain}/templates`, {
+      name: templateName,
+      description,
+      template: htmlTemplate,
+    });
+  };
+
+  private putTemplateVersion = async (
+    templateName: string,
+    versionTag: string,
+    htmlTemplate: string
+  ) => {
+    return this.put(`/${this.domain}/templates/${templateName}/versions/${versionTag}`, {
+      template: htmlTemplate,
+      version: versionTag,
+    });
+  };
+
+  upsertTemplate = async (
+    templateName: string,
+    description: string,
+    versionTag: string,
+    htmlTemplate: string
+  ) => {
+    try {
+      await this.putTemplateVersion(templateName, versionTag, htmlTemplate);
+    } catch {
+      await this.postTemplate(templateName, description, htmlTemplate);
+    }
+  };
+}
